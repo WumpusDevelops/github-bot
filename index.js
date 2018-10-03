0bb7dd455d00d11a7380c01847e76653910e7218
Kolkies <kolkies@kolkiesdev.nl>
2018-10-03 07:46:45 +0200

    Started with testing the github bot

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// TODO
+require("dotenv").config();
+
+const Client = require("./struc/GitHubClient");
+const app = require("express")();
+
+const createCommentonPullrequest = async () => {
+    const pr = await Client.pullRequests.get({owner: "DiscordManager", repo: "github-bot-testing", number: "1"});
+    const createComment = await Client.pullRequests.createComment({owner: "DiscordManager", repo: "github-bot-testing", number: pr.data.number, body: `Thanks for that pullrequest ${pr.data.user.login}!`, commit_id: pr.data.merge_commit_sha})
+    console.log(createComment);
+};
+createCommentonPullrequest();
+
+// app.post("/pulladded", async (req, res) => {
+//     const pr = await Client.pullRequests.get({owner: "DiscordManager", repo: "github-bot-testing", number: "1"});
+//     console.log(pr);
+//     res.end();
+// });
+
+// app.listen(8000);
