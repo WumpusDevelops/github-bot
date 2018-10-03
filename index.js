ff42a19e2c04646254000c877be9e6230dbc0817
Kolkies <kolkies@kolkiesdev.nl>
2018-10-03 07:49:30 +0000

    Getting started with own lib

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 require("dotenv").config();
 
-const Client = require("./struc/GitHubClient");
 const app = require("express")();
 
 const createCommentonPullrequest = async () => {
-    const pr = await Client.pullRequests.get({owner: "DiscordManager", repo: "github-bot-testing", number: "1"});
-    const createComment = await Client.pullRequests.createComment({owner: "DiscordManager", repo: "github-bot-testing", number: pr.data.number, body: `Thanks for that pullrequest ${pr.data.user.login}!`, commit_id: pr.data.merge_commit_sha})
-    console.log(createComment);
+    require("./lib/createComment")(1, "hi");
 };
 createCommentonPullrequest();
 
-// app.post("/pulladded", async (req, res) => {
-//     const pr = await Client.pullRequests.get({owner: "DiscordManager", repo: "github-bot-testing", number: "1"});
-//     console.log(pr);
-//     res.end();
-// });
+/* app.post("/pulladded", async (req, res) => {
+    const pr = await Client.pullRequests.get({owner: "DiscordManager", repo: "github-bot-testing", number: "1"});
+    console.log(pr);
+    res.end();
+});
 
-// app.listen(8000);
+app.listen(8000); */
