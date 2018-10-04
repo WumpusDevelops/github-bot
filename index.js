3b79f8f667550d88cc308a253a1c8649ac1302ed
Kolkies <kolkies@kolkiesdev.nl>
2018-10-04 08:50:22 +0200

    Fix everything, should work now

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const createCommit = require("./lib/createCommit");
 const createComment = require("./lib/createComment");
 const addLabeltoPr = require("./lib/addLabeltoPr");
 const bumpVersion = require("./lib/bumpVersion");
+const { base64encode     } = require('nodejs-base64');
 
 const app = require("express")();
 
@@ -18,18 +19,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.post("/pullrequest", async (req, res) => {
-    const { body } = req;
-    console.log(body);
-    if(body.action == "closed" && body.merged == true) {
-
-        
-    }
-    if(body.action == "opened") {
+    let { body } = req;
+    if(body.payload) body = JSON.parse(body.payload);
+    if(body.action == "closed" && body.pull_request.merged == true) {
+        const data = await getPackageJsonVersion();
+        const bumpedversion = bumpVersion(data.version);
+        let content = { version: bumpedversion };
+        let contentencrypted = base64encode(JSON.stringify(content));
+        await createCommit("package.json", contentencrypted, data.sha)
+        return res.status(200).end();
+    } else if(body.action == "opened") {
         await createComment(body.number, `Thanks for opening this Pull Request, **${body.pull_request.user.login}**!\nI will add the language label to this Pull Request and it will be reviewed as soon as possible!\nYou can also join [our Discord Server](https://discord.gg/Japq4P) and you can get the Translator role!`)
         await addLabeltoPr(body.number);
         return res.status(200).end();
-    }
-    return res.status(200).end();
+    } else return res.status(400).end();
 });
 app.get("/", (req, res) => {
     res.send("Hi!");
